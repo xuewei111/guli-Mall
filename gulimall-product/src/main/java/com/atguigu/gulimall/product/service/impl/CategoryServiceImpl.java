40f61b54782b141202db1c870748148bdc18efce
xuewei111 <m13358109269_1@163.com>
2022-09-04 22:30:47 +0800

    gateway 转发配置

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1. 查出所有的分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        // 2.组装成父子的树形结构
+        List<CategoryEntity> collect = entities.stream()
+                .filter(item -> item.getParentCid().longValue() == 0)
+                .map(item -> {
+                    item.setChildren(getChildrens(item,entities));
+                    return item;
+                })
+                .sorted(Comparator.comparingInt(a -> (a.getSort() == null ? 0 : a.getSort())))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    // 递归查询所有菜单的子菜单
+    private List<CategoryEntity> getChildrens(CategoryEntity category,List<CategoryEntity> categoryEntityList) {
+        List<CategoryEntity> collect = categoryEntityList.stream()
+                .filter(item -> item.getParentCid().equals(category.getCatId()))
+                .map(item -> {
+                    item.setChildren(getChildrens(item, categoryEntityList));
+                    return item;
+                })
+                .sorted(Comparator.comparingInt(a -> (a.getSort() == null ? 0 : a.getSort())))
+                .collect(Collectors.toList());
+        return collect;
     }
 
 }
