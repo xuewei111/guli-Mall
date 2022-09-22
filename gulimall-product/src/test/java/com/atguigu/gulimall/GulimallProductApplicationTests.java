31ebc1d4064c2bd7eb366d580a16466cffc41414
薛巍 <xuew@kaituo.com.cn>
2022-09-22 23:45:25 +0800

    文件服务器OSS配置

--- gulimall-product/src/test/java/com/atguigu/gulimall/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/GulimallProductApplicationTests.java
@@ -1,10 +1,76 @@
 package com.atguigu.gulimall;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
+/**
+ * 1.引入oss-starter
+ * 2.配置key,endpoint相关信息即可
+ * 3.使用OSSClient 进行相关操作
+ */
 @SpringBootTest
 class GulimallProductApplicationTests {
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Test
+    public void testUpload() {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-shanghai.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI5tCuhAuLA1YUxGybSZY6";
+//        String accessKeySecret = "8L0Cvs4K68zsgKEkP17oQra3eo2tqh";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "xuewei-gulimall";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "xuewei-gulimall/AZ5V0472.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+        String filePath= "/Users/xuewei/Downloads/AZ5V0472.jpg";
+
+        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传文件。
+            ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
 
     @Test
     void contextLoads() {
